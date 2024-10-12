20e88b2d56bc71d09b3da76b5bee72e850b69ec6
lufer <lufer@ipca.pt>
2024-10-12 15:58:17 +0100

    Métodos de classe e de instância

---
+++ Aulas/Aula 05/Calculadora.cs
@@ -0,0 +1,108 @@
+﻿/*
+*	<copyright file="Calculadora.cs" company="IPCA">
+*		Copyright (c) 2024 All Rights Reserved
+*	</copyright>
+* 	<author>lufer</author>
+*   <date>10/8/2024 3:36:02 PM</date>
+*	<description></description>
+**/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Aula_05
+{
+    /// <summary>
+    /// Purpose:
+    /// Created by: lufer
+    /// Created on: 10/8/2024 3:36:02 PM
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    public class Calculadora
+    {
+        #region Attributes
+        int x;
+        int y;
+        int resultado;
+        
+        #endregion
+
+        #region Methods
+
+        #region Constructors
+
+        /// <summary>
+        /// The default Constructor.
+        /// </summary>
+        public Calculadora()
+        {
+        }
+
+        public Calculadora(int x, int y)
+        {
+            //this.x = x;
+            X = x;
+            Y = y;
+        }
+
+        #endregion
+
+        #region Properties
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public int Resultado
+        {
+            get { return resultado; }
+            set { resultado = value; }
+        }
+
+        #endregion
+
+        #region Functions
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int Soma()
+        {
+            return x + y;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        #endregion
+
+        #region Overrides
+        #endregion
+
+        #region Destructor
+        /// <summary>
+        /// The destructor.
+        /// </summary>
+        ~Calculadora()
+        {
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Aulas/Aula 05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora a = new Calculadora();
+            a.X = 1;
+            a.Y = 2;
+
+            int resultado = a.Soma();
+            int resultado2 = a.Soma(3,7);
+        }
+    }
+}
