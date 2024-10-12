972e05c1df6543e46daef556a0bb4a145d5fb5b4
lufer <lufer@ipca.pt>
2024-10-12 15:55:45 +0100

    Projetos e Soluções

---
+++ Aulas/Aula 01/Program.cs
@@ -0,0 +1,58 @@
+﻿/**
+ * POO - LESI
+ * lufer
+ * lufer@ipca.pt
+ * 24-06-2024
+ * description: C Review
+ * */
+
+using System;
+
+namespace Aula_01
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Program
+    {
+
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            #region XX
+
+            //Data types e variables
+            int x;
+
+            x = 23;
+
+            x++;
+
+            int y = x * 2;
+
+            //Instructions
+            //if, do, while, for, foreach
+
+            //Operators
+            //logic: >=, !=; ==; <=; 
+            //Cnditrional: :?
+
+            x = (y > 10 ? 7 : 8);
+
+            Console.WriteLine("X=" + x.ToString());    //printf
+            Console.WriteLine("X={0}", x);  //printf ("%d",x);
+
+            #endregion
+
+
+
+        }
+
+
+    }
+}
