b54c3ff0ed2f4f2bcceed075b46c987ccf821934
lufer <lufer@ipca.pt>
2024-10-12 15:57:57 +0100

    Propriedades e Construtores

---
+++ Aulas/Aula 04/Aluno.cs
@@ -0,0 +1,123 @@
+﻿/*
+*	<copyright file="Aula_04.cs" company="IPCA">
+*		Copyright (c) 2024 All Rights Reserved
+*	</copyright>
+* 	<author>lufer</author>
+*   <date>10/3/2024 10:23:54 AM</date>
+*	<description>Classe para </description>
+**/
+using System;
+
+namespace Aula_04
+{
+    /// <summary>
+    /// Purpose:
+    /// Created by: lufer
+    /// Created on: 10/3/2024 10:23:54 AM
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    public class Aluno
+    {
+        #region Attributes
+
+        string nomePessoa;
+        int idade;
+        string morada;          //definir propriedade...
+
+        #endregion
+
+        #region Methods
+
+        #region A_LA_JAVA
+        public bool DefineNome(string n)
+        {
+            nomePessoa = n;
+            return true;
+        }
+
+        public string QualNome()
+        {
+            return nomePessoa.ToUpper();
+        }
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// The default Constructor.
+        /// </summary>
+        public Aluno()
+        {
+            Nome = "";
+            idade = 0;
+            morada = "Barcelos";
+        }
+
+        public Aluno(string nome)
+        {
+            //nomePessoa=nome;
+            Nome = nome;
+            idade = 0;
+        }
+
+        public Aluno(string nome, int idade)
+        {
+            //nomePessoa=nome;
+            Nome = nome;
+            //this.idade= idade;
+            Idade = idade;
+
+        }
+
+        #endregion
+
+        #region Properties
+        public string Nome
+        {
+            //=>
+            get { return nomePessoa.ToUpper(); }
+            set { 
+                if(value.Length>0)
+                    nomePessoa = value; 
+            }
+        }
+
+        public int Idade
+        {
+            set { if(value>0 && value<200) idade = value; }
+        }
+
+        #endregion
+
+
+
+        #region Overrides
+        #endregion
+
+        #region OtherMethods
+
+        public DateTime DataNascimento()
+        {
+            return DateTime.Today;
+        }
+
+        public string Regiao()
+        {
+            return "";
+        }
+        #endregion
+
+        #region Destructor
+        /// <summary>
+        /// The destructor.
+        /// </summary>
+        ~Aluno()
+        {
+            //
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Aulas/Aula 04/Program.cs
@@ -0,0 +1,40 @@
+﻿/**/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a;        //declarar objeto
+
+            a = new Aluno(); //objeto do tipo Aluno
+            //a.nome = "ok";      //EVITAR!!!!
+
+
+            a.DefineNome("ok");
+            Console.Write("Nome a:" + a.QualNome());
+
+            a.Nome = "ok1";
+            Console.Write("Nome a:" + a.Nome);
+
+            Aluno b = new Aluno();
+            b.Idade = 23;
+            b.Nome = "oli";
+
+            Aluno d = new Aluno("ok");
+            d.Idade = 12;
+
+            Aluno e = new Aluno("ola", 27);
+
+
+
+        }
+    }
+}
