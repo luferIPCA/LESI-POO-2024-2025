f41146e703b4f687bdc7baf99eee329f2d38fa21
lufer <lufer@ipca.pt>
2024-11-12 11:37:43 +0000

    Pilares de POO

---
+++ Aulas/Aula 07 - Pillars/Bike.cs
@@ -0,0 +1,66 @@
+﻿/*
+*	<copyright file="Aula_07_Pilars.cs" company="IPCA">
+*		Copyright (c) 2024 All Rights Reserved
+*	</copyright>
+* 	<author>mustl</author>
+*   <date>10/28/2024 3:33:11 PM</date>
+*	<description></description>
+**/
+using System;
+
+namespace Aula_07_Pilars
+{
+    /// <summary>
+    /// Purpose:
+    /// Created by: mustl
+    /// Created on: 10/28/2024 3:33:11 PM
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    public class Bike: Veiculo
+    {
+        #region Attributes
+        public int anoB;     //ATENÇÃO
+        #endregion
+
+        #region Methods
+
+        #region Constructors
+
+        /// <summary>
+        /// The default Constructor.
+        /// </summary>
+        public Bike()
+        {
+            Console.WriteLine("Nova Bike");
+        }
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+
+
+        #region Overrides
+        #endregion
+
+        #region OtherMethods
+        public new int QualAno()
+        {
+            return AnoVeiculo.Year;
+        }
+        #endregion
+
+        #region Destructor
+        /// <summary>
+        /// The destructor.
+        /// </summary>
+        ~Bike()
+        {
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Aulas/Aula 07 - Pillars/OutroVeiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_07_Pilars
+{
+    public class OutroVeiculo : IVeiculo
+    {
+        public double Celindrada(int x)
+        {
+            return x * 3;
+        }
+    }
+}
